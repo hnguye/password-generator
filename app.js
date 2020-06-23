b9678a839c9e0f8345e8ce2d8b87d5da844b6d64
hnguye <hieutnguyen06@gmail.com>
2020-06-23 21:36:50 +1000

    Generate password

--- app.js
+++ app.js
@@ -30,7 +30,31 @@ generate.addEventListener('click', () => {
 
 	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber,
 		hasSymbol, length);
-});
+})
+
+function generatePassword(lower, upper, number, symbol, length) {
+	let generatedPassword = '';
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item =>
+		Object.values(item)[0]);
+
+	// Doesn't have a selected type
+	if (typesCount === 0) {
+		return '';
+	}
+
+	// create a loop
+	for (let i = 0; i < length; i += typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+
+	const finalPassword = generatedPassword.slice(0, length);
+
+	return finalPassword;
+};
 
 const randomFunc = {
 	lower: getRandomLower,
