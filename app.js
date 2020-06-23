f8c7de47b7bf5d31d6d2f86d9db62a3716229140
hnguye <hieutnguyen06@gmail.com>
2020-06-23 21:35:49 +1000

    Listen to events on clipboard

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboard = document.getElementById('clipboard');
 
+clipboard.addEventListener('click', () => {
+	const textarea = document.createElement('textarea');
+	const password = resultEl.innerText;
+
+	if (!password) { return; }
+
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password copied to clipboard');
+});
+
 
 const randomFunc = {
 	lower: getRandomLower,
