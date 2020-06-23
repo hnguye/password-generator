8c30c91942f0052a7b73b776103ac922a80ec0cd
hnguye <hieutnguyen06@gmail.com>
2020-06-23 21:32:56 +1000

    Grouping random functions to object

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomNUmber,
+	number: getRandomNUmber,
+	symbol: getRandomSymbol,
+};
+
 function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 function getRandomNUmber() {
-	return String.fromCharCode(Math.floor(Math.random() * 10) + 40)
+	return String.fromCharCode(Math.floor(Math.random() * 10) + 40);
 }
 
 function getRandomSymbol() {
-	const symbols = '!@#$%^&*()[]{}_-=><,.'
-	return symbols[Math.floor(Math.random() * symbols.length)]
+	const symbols = '!@#$%^&*()[]{}_-=><,.';
+	return symbols[Math.floor(Math.random() * symbols.length)];
 }
